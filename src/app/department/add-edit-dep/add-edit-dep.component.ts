92c7b665adb16e41d2217709eb6534255fc95892
Ricardo <ricardomaciel1597@gmail.com>
2024-06-14 12:36:54 -0300

    first commit

---
+++ src/app/department/add-edit-dep/add-edit-dep.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-dep',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-edit-dep.component.html',
+  styleUrl: './add-edit-dep.component.css'
+})
+export class AddEditDepComponent {
+
+}
